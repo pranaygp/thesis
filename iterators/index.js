015511955a86193f65b2c15ff4af84e71256d6cf
Thomas Fischer <thomasfischer@fastmail.com>
2018-02-26 14:10:19 -0600

    add basic iterators/ examples

---
+++ iterators/index.js
@@ -0,0 +1,70 @@
+class Iterator {}
+
+Array.prototype.toIter = function() {
+    let nextIndex = 0;
+    const it = new Iterator();
+    it.next = () => nextIndex < this.length ?
+        {value: this[nextIndex++], done: false} : {done: true}
+    return it;
+}
+
+const toIterExample = () => {
+    let xs   = Array.from(new Array(10), (x, i) => i + 1);
+    let xsIt = xs.toIter();
+
+    while (true) {
+        let maybeVal = xsIt.next();
+        if (maybeVal.done) break;
+        console.log(maybeVal.value);
+    }
+}
+
+toIterExample()
+
+Iterator.prototype.map = function(f) {
+    const it = new Iterator();
+    it.next = () => {
+        const maybeVal = this.next();
+        if (maybeVal.done) return {done: true};
+        return {value: f(maybeVal.value), done: false};
+    }
+    return it;
+}
+
+Iterator.prototype.filter = function(f) {
+    const it = new Iterator();
+    it.next = () => {
+        while (true) {
+            const maybeVal = this.next();
+            if (maybeVal.done) return {done: true};
+            if (!f(maybeVal.value)) continue;
+            return {value: maybeVal.value, done: false};
+        }
+    }
+    return it;
+}
+
+Iterator.prototype.collect = function() {
+    let ret = [];
+    while (true) {
+        const maybeVal = this.next();
+        if (maybeVal.done) break;
+        ret.push(maybeVal.value);
+    }
+    return ret;
+}
+
+const mapCollectExample = () => {
+    let xs = Array.from(new Array(10), (x, i) => i + 1);
+
+    let myIt = xs.toIter()
+        .map(x => x*x)
+        .filter(x => x % 2 == 0)
+        .map(x => x*x)
+        .filter(x => x % 2 == 0)
+        .map(x => x*x)
+        .filter(x => x % 2 == 0)
+        .collect();
+}
+
+mapCollectExample();
