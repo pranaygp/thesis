1169b2bf882c82739edc752c913b9d7c20f47ca9
Pranay Prakash <pranay.gp@gmail.com>
2018-04-17 02:27:37 -0500

    join -> R.unnest

--- __mocks__/deforest.js
+++ __mocks__/deforest.js
@@ -1,18 +1,18 @@
 const R = require('ramda');
 
 // Convert an array of chars to a string
-// const stringify = xs => xs.join('')
-const stringify = x => x
+const stringify = xs => xs.join('')
 
 const from = (x, y) => R.unfold(n => n <= y && [n, n+1], x)
 const upto = x => from(1, x)
 
-const repeat = x => R.repeat(x, 10)
+const repeat = x => R.repeat(x, 1e4)
 
 module.exports = {
   ...R,
   from,
   upto,
   repeat,
+  join: R.unnest,
   stringify
 }
